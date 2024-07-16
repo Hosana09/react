05e99360999c273eb3aa2f6620614840344e74a2
Hosana09 <hosanapires9@gmail.com>
2024-07-16 13:25:24 -0300

    primeiro commit

---
+++ hello-world-jsx/src/App.jsx
@@ -0,0 +1,17 @@
+//components
+import FirstComponent from './components/FirstComponent';
+
+//style
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World React!</h1>
+      <p>Sim, sou a melhor programadora em React de todos os tempos!</p>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
